a78093c6cb3fe1e398f50e6da711a74391a3130b
7 Krysta <rokalykriszta@gmail.com>
2023-03-15 14:33:22 +0200

    Fix issue with subtract method.

--- isp-lab-1-master/isp-lab-2-2023-master/src/main/java/isp/lab2/Exercise4StringSearch/Exercise4StringSearch.java
+++ isp-lab-1-master/isp-lab-2-2023-master/src/main/java/isp/lab2/Exercise4StringSearch/Exercise4StringSearch.java
@@ -1,5 +1,7 @@
 package isp.lab2.Exercise4StringSearch;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercise4StringSearch {
@@ -10,7 +12,18 @@ public class Exercise4StringSearch {
      * @return
      */
     public static String[] searchSubstrings(String input, String substring) {
-        return null;
+        ArrayList<String> inputList = new ArrayList<>(Arrays.asList(input.split(",")));
+        ArrayList<String> resultList = new ArrayList<>();
+        for (String word : inputList) {
+            if (word.contains(substring)) {
+                resultList.add(word);
+            }
+        }
+        String[] result = new String[resultList.size()];
+        for (int i = 0; i < resultList.size(); ++i) {
+            result[i] = resultList.get(i);
+        }
+        return result;
     }
 
     public static String readFromConsoleInt() {
