2b50b6ee1b4d448011b8d6e73123eea82e8c3cc4
7 Krysta <rokalykriszta@gmail.com>
2023-05-10 14:49:10 +0300

    lab8createroute

--- isp-lab-8-2023-main/src/main/java/isp/lab8/airways/Exercise.java
+++ isp-lab-8-2023-main/src/main/java/isp/lab8/airways/Exercise.java
@@ -1,7 +1,69 @@
 package isp.lab8.airways;
 
-public class Exercise {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Exercise {
+
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the name of the new route: ");
+        String routeName = input.nextLine();
+
+        String packagePath = "isp.lab8.airways." + routeName;
+        Package routePackage = Package.getPackage(packagePath);
+        if (routePackage == null) {
+            routePackage = createPackage(packagePath);
+        }
+
+        ArrayList<String> waypoints = new ArrayList<String>();
+        boolean addAnother = true;
+        while (addAnother) {
+            System.out.print("Enter the name of a waypoint (or 'done' to finish): ");
+            String waypoint = input.nextLine();
+            if (waypoint.equalsIgnoreCase("done")) {
+                addAnother = false;
+            } else {
+                waypoints.add(waypoint);
+            }
+        }
+
+        for (int i = 0; i < waypoints.size(); i++) {
+            String waypointName = waypoints.get(i);
+            String packageName = "waypoint_" + i;
+            String packagePathWithWaypoint = packagePath + "." + packageName;
+            Package waypointPackage = Package.getPackage(packagePathWithWaypoint);
+            if (waypointPackage == null) {
+                Package parentPackage = Package.getPackage(packagePath + ".waypoint_" + (i - 1));
+                if (parentPackage == null) {
+                    throw new Exception("Parent package does not exist.");
+                }
+                waypointPackage = createPackage(packagePathWithWaypoint, parentPackage);
+            }
+        }
+
+        System.out.println("Route and waypoints saved successfully.");
+    }
+
+    private static Package createPackage(String packageName, Package parentPackage) throws Exception {
+        String[] packageNames = packageName.split("\\.");
+        return (Package) ClassLoader.getSystemClassLoader()
+                .loadClass(parentPackage.getName() + "." + packageNames[packageNames.length - 1])
+                .getDeclaredConstructor()
+                .newInstance();
+    }
+
+    private static Package createPackage(String packageName) throws Exception {
+        String[] packageNames = packageName.split("\\.");
+        String parentPackageName = "";
+        for (int i = 0; i < packageNames.length - 1; i++) {
+            parentPackageName += packageNames[i] + ".";
+        }
+        Package parentPackage = Package.getPackage(parentPackageName);
+        if (parentPackage == null) {
+            throw new Exception("Parent package does not exist.");
+        }
+        return createPackage(packageName, parentPackage);
     }
 }
--- isp-lab-8-2023-main/src/main/java/isp/lab8/airways/Waypoint.java
+++ isp-lab-8-2023-main/src/main/java/isp/lab8/airways/Waypoint.java
@@ -9,4 +9,44 @@ public class Waypoint {
     private double latitude;
     private double longitude;
     private int altitude;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public int getAltitude() {
+        return altitude;
+    }
+
+    public void setAltitude(int altitude) {
+        this.altitude = altitude;
+    }
 }
